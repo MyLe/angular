192691c791e18ac896c2702e76254ed0cb287105
Hien Le <roh7463@gmail.com>
2020-06-07 23:47:01 +0700

    frist commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import {RouterModule} from '@angular/router'
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { UserloginComponent } from './userlogin/userlogin.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
+import { SiderbarComponent } from './siderbar/siderbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    UserloginComponent,
+    MainpageComponent,
+    SiderbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {
+         path: 'userlogin',
+         component: UserloginComponent
+      },
+      {
+        path: 'mainpage',
+        component: MainpageComponent
+     }
+   ])
   ],
   providers: [],
   bootstrap: [AppComponent]
